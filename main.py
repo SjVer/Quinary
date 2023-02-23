3812e52c8f57fd604b5f0a19c0b68cad14ff24d0
SjVer <sjoerd@marsenaar.com>
2023-02-23 21:19:59 +0100

    working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import List, Tuple, Literal, Optional
 from dataclasses import dataclass
 from json import load
 
-FILE_FORMAT = "segments/{digit}/{digit}-{part}.svg"
+FILE_FORMAT = "segments/{digit}/{digit}-{part}{conn}.svg"
 OUTPUT_FORMAT = "out/{number}.svg"
 HEIGHT = 100
 MARGIN = 20
@@ -18,9 +18,10 @@ class Segment:
 	part: Literal['l', 'm', 'r']
 	"""
 	conn = 'b': e.g. 1->1, bottom (horizontal)
-	conn = 'z': e.g. 0->2, zero (double horizontal)
-	conn = 'n': normal
-	conn = 'm': middle
+	conn = 't': e.g. 2->4, top (horizontal)
+	conn = 'z': e.g. 0->2, zero (double top)
+	conn = 'c': e.g. 1->2, cut off to merge better
+	conn = None: normal
 	"""
 	conn: Optional[Literal['t', 'b', 'z', 'n', 'm']]
 
@@ -60,18 +61,22 @@ def select_segments(digits: str) -> List[Segment]:
 
 		# set segments of current digit
 		segments.append(Segment(digit, 'l', curr_l_conn))
-		segments.append(Segment(digit, 'm', 'm'))
+		segments.append(Segment(digit, 'm', 'n'))
 		segments.append(Segment(digit, 'r', 'n'))
 
 	for s in segments: print(s)
 	return segments
 
-def render(segments: List[Segment]):
+def render(number: str, segments: List[Segment]):
 	# get segment figures
-	seg_figs = [
-		fromfile(FILE_FORMAT.format(digit=seg.digit, part=seg.part))
-		for seg in segments
-	]
+	seg_figs: List[SVGFigure] = []
+	for seg in segments:
+		if seg.conn:
+			seg_figs.append(fromfile(FILE_FORMAT.format(
+				digit = seg.digit,
+				part = seg.part,
+				conn = "-" + seg.conn if seg.conn != 'n' else ''
+			)))
 
 	# find total width	
 	width = 0
@@ -90,31 +95,31 @@ def render(segments: List[Segment]):
 		root.root.attrib["fill"] = "blue"
 		root.moveto(x, MARGIN)
 		fig.append(root)
-		x += int(seg.width.removesuffix("mm")) - 0.1
+		x += int(seg.width.removesuffix("mm")) - 0.15
 
-	fig.save(OUTPUT_FORMAT.format(number = "2"))
-	print("saved to:", OUTPUT_FORMAT.format(number = "2"))
+	fig.save("out.svg")
+	print("saved to: out.svg")
+	# fig.save(OUTPUT_FORMAT.format(number = number))
+	# print("saved to:", OUTPUT_FORMAT.format(number = number))
 
 def main():
 	# get input (and convert to quinary if needed)
-	# digits = input("value (start with '0q' for quinary): ")
-	# if digits.startswith("0q"):
-	# 	for c in digits[2:]:
-	# 		if c not in DIGITS:
-	# 			print(f"invalid quinary digit '{c}'")
-	# 			exit(1)
-	# 	digits = digits[2:]
-	# else:
-	# 	digits = dec_to_quin(int(digits))
-	# 	print(f"converted to quinary: 0q{digits}")
-
-	digits = '1212'
+	digits = input("value (start with '0q' for quinary): ")
+	if digits.startswith("0q"):
+		for c in digits[2:]:
+			if c not in DIGITS:
+				print(f"invalid quinary digit '{c}'")
+				exit(1)
+		digits = digits[2:]
+	else:
+		digits = dec_to_quin(int(digits))
+		print(f"converted to quinary: 0q{digits}")
 
 	# select svg segments
 	segments = select_segments(digits)
 
 	# render final svg
-	render(segments)
+	render(digits, segments)
 
 if __name__ == "__main__":
   main()
